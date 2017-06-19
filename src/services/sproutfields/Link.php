82f9911c085b10eefd32ed30230ee823f55e0d04
Andre Lopez <andre@barrelstrengthdesign.com>
2017-06-19 19:24:13 -0400

    Adds Link service

---
+++ src/services/sproutfields/Link.php
@@ -0,0 +1,66 @@
+<?php
+namespace barrelstrength\sproutcore\services\sproutfields;
+
+use craft\base\Field;
+use yii\base\Component;
+use Craft;
+use barrelstrength\sproutcore\Module;
+
+/**
+ * Class LinkService
+ *
+ */
+class Link extends Component
+{
+	/**
+	 * Validates a phone number against a given mask/pattern
+	 *
+	 * @param $value
+	 * @param Field $field
+	 *
+	 * @return bool
+	 */
+	public function validate($value, Field $field): bool
+	{
+		$customPattern = $field->customPattern;
+		$checkPattern  = $field->customPatternToggle;
+
+		if ($customPattern && $checkPattern)
+		{
+			// Use backticks as delimiters as they are invalid characters for emails
+			$customPattern = "`" . $customPattern . "`";
+
+			if (preg_match($customPattern, $value))
+			{
+				return true;
+			}
+		}
+		else
+		{
+			if ((!filter_var($value, FILTER_VALIDATE_URL) === false))
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Return error message
+	 *
+	 * @param  mixed  $field
+	 *
+	 * @return string
+	 */
+	public function getErrorMessage($fieldName, $field): string
+	{
+		if (!empty($field->customPattern) && isset($field->customPatternErrorMessage))
+		{
+			return Module::t($field->customPatternErrorMessage);
+		}
+
+		return Module::t($fieldName . ' must be a valid link.');
+	}
+
+}
