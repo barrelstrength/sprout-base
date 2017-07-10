3c1b41f9e5cecd4fc85b3d6748a865ddffdbf359
Dale Ramirez <dale@barrelstrengthdesign.com>
2017-07-10 19:09:34 +0800

    Added SproutCoreAsset class that include report results css style.

---
+++ src/web/assets/sproutreports/ReportCoreAsset.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace barrelstrength\sproutcore\web\assets\sproutreports;
+
+use craft\web\AssetBundle;
+use craft\web\assets\cp\CpAsset;
+
+class ReportCoreAsset extends AssetBundle
+{
+	public function init()
+	{
+		$this->sourcePath = "@sproutcore/resources/sproutreports";
+
+		$this->depends = [
+			CpAsset::class,
+		];
+
+		$this->css = [
+			'css/styles.css',
+			'css/sproutreports.min.css'
+		];
+
+		parent::init();
+	}
+}
