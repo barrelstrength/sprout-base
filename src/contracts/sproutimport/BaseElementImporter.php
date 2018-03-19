97bb9b65a4120f9d91dd49f1de278514acbf176e
Ben Parizek <ben@barrelstrengthdesign.com>
2018-03-19 00:10:24 -0400

    Adds getMockData to BaseElementImporter

--- src/contracts/sproutimport/BaseElementImporter.php
+++ src/contracts/sproutimport/BaseElementImporter.php
@@ -262,6 +262,16 @@ abstract class BaseElementImporter extends BaseImporter
         }
     }
 
+    /**
+     * @param $quantity
+     * @param $settings
+     *
+     * @return array|null
+     */
+    public function getMockData($quantity, $settings) {
+        return null;
+    }
+
     private function isDateAttribute($handle)
     {
         $dates = ['postDate', 'dateCreated', 'dateUpdated'];
