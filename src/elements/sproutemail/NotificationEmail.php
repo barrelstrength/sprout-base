9ded53e4ab16818553307a0956133b9c492c3404
Ben Parizek <ben@barrelstrengthdesign.com>
2018-04-14 18:10:22 -0400

    Moves template options to variable

--- src/elements/sproutemail/NotificationEmail.php
+++ src/elements/sproutemail/NotificationEmail.php
@@ -507,60 +507,6 @@ class NotificationEmail extends Element
     }
 
     /**
-     * Get the available Email Template Options
-     *
-     * @return array
-     */
-    public function getTemplateOptions()
-    {
-        $defaultEmailTemplates = new BasicTemplates();
-        $templates = SproutBase::$app->template->getAllGlobalTemplates();
-        $templateIds = [];
-        $options = [
-            [
-                'label' => Craft::t('sprout-base', 'Select...'),
-                'value' => ''
-            ]
-        ];
-
-        // Build our options
-        foreach ($templates as $template) {
-            $options[] = [
-                'label' => $template->getName(),
-                'value' => $template->getTemplateId()
-            ];
-            $templateIds[] = $template->getTemplateId();
-        }
-
-        $templateFolder = null;
-        $plugin = Craft::$app->getPlugins()->getPlugin('sprout-email');
-
-        if ($plugin) {
-            $settings = $plugin->getSettings();
-        }
-
-        $templateFolder = $this->template ?? $settings->templateFolderOverride ?? $defaultEmailTemplates->getPath();
-
-        $options[] = [
-            'optgroup' => Craft::t('sprout-base', 'Custom Template Folder')
-        ];
-
-        if (!in_array($templateFolder, $templateIds, false) && $templateFolder != '') {
-            $options[] = [
-                'label' => $templateFolder,
-                'value' => $templateFolder
-            ];
-        }
-
-        $options[] = [
-            'label' => Craft::t('sprout-base', 'Add Custom'),
-            'value' => 'custom'
-        ];
-
-        return $options;
-    }
-
-    /**
      * @param mixed|null $element
      *
      * @throws \Exception
--- src/web/twig/variables/SproutBaseVariable.php
+++ src/web/twig/variables/SproutBaseVariable.php
@@ -7,7 +7,9 @@
 
 namespace barrelstrength\sproutbase\web\twig\variables;
 
+use barrelstrength\sproutbase\integrations\emailtemplates\BasicTemplates;
 use barrelstrength\sproutbase\SproutBase;
+use Craft;
 
 class SproutBaseVariable
 {
@@ -43,5 +45,59 @@ class SproutBaseVariable
     public function getCountries()
     {
         return SproutBase::$app->phone->getCountries();
+    }
+
+    /**
+     * Get the available Email Template Options
+     *
+     * @return array
+     */
+    public function getEmailTemplateOptions()
+    {
+        $defaultEmailTemplates = new BasicTemplates();
+        $templates = SproutBase::$app->template->getAllGlobalTemplates();
+        $templateIds = [];
+        $options = [
+            [
+                'label' => Craft::t('sprout-base', 'Select...'),
+                'value' => ''
+            ]
+        ];
+
+        // Build our options
+        foreach ($templates as $template) {
+            $options[] = [
+                'label' => $template->getName(),
+                'value' => $template->getTemplateId()
+            ];
+            $templateIds[] = $template->getTemplateId();
+        }
+
+        $templateFolder = null;
+        $plugin = Craft::$app->getPlugins()->getPlugin('sprout-email');
+
+        if ($plugin) {
+            $settings = $plugin->getSettings();
+        }
+
+        $templateFolder = $this->template ?? $settings->templateFolderOverride ?? $defaultEmailTemplates->getPath();
+
+        $options[] = [
+            'optgroup' => Craft::t('sprout-base', 'Custom Template Folder')
+        ];
+
+        if (!in_array($templateFolder, $templateIds, false) && $templateFolder != '') {
+            $options[] = [
+                'label' => $templateFolder,
+                'value' => $templateFolder
+            ];
+        }
+
+        $options[] = [
+            'label' => Craft::t('sprout-base', 'Add Custom'),
+            'value' => 'custom'
+        ];
+
+        return $options;
     }
 }
