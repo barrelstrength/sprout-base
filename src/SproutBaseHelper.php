66bfc7025011c0377dcfedbcf0210f30657c2f39
Andre L <andrelopezd@gmail.com>
2018-05-10 23:06:53 -0400

    Fixes bug when using cli.
    https://github.com/barrelstrength/craft-sprout-forms/issues/91

--- src/SproutBaseHelper.php
+++ src/SproutBaseHelper.php
@@ -19,8 +19,14 @@ abstract class SproutBaseHelper
     public static function registerModule()
     {
         if (!Craft::$app->hasModule('sprout-base')) {
+            $module = SproutBase::class;
 
-            Craft::$app->setModule('sprout-base', SproutBase::class);
+            if (Craft::$app->getRequest()->isConsoleRequest){
+                // @todo - research why this is causing problems when using the craft console.
+                $module = null;
+            }
+
+            Craft::$app->setModule('sprout-base', $module);
 
             // Have Craft load this module right away (so we can create templates)
             Craft::$app->getModule('sprout-base');
