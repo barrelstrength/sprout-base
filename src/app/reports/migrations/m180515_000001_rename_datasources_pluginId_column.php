8b51dba82c9dd0b87950224fab512b53b1dde5ab
Ben Parizek <ben@barrelstrengthdesign.com>
2018-05-16 00:01:07 -0400

    Adds migration for updarted datasource class and column names

---
+++ src/app/reports/migrations/m180515_000001_rename_datasources_pluginId_column.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace barrelstrength\sproutbase\app\reports\migrations;
+
+use craft\db\Migration;
+
+/**
+ * m180515_000000_rename_datasources_pluginId_column migration.
+ */
+class m180515_000001_rename_datasources_pluginId_column extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $table = '{{%sproutreports_datasources}}';
+
+        if ($this->db->columnExists($table, 'pluginId')) {
+            $this->renameColumn($table, 'pluginId', 'pluginHandle');
+        }
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m180515_000000_rename_datasources_pluginId_column cannot be reverted.\n";
+        return false;
+    }
+}
