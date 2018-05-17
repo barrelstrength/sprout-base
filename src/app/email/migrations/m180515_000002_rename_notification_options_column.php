393b83388f87091eb848e9a250ed64799e0d73e5
Ben Parizek <ben@barrelstrengthdesign.com>
2018-05-16 23:32:31 -0400

    Adds migration to rename Notification Email options column to settings

---
+++ src/app/email/migrations/m180515_000002_rename_notification_options_column.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace barrelstrength\sproutbase\app\email\migrations;
+
+use craft\db\Migration;
+
+/**
+ * m180515_000002_rename_notification_options_column migration.
+ */
+class m180515_000002_rename_notification_options_column extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $table = '{{%sproutemail_notificationemails}}';
+
+        if ($this->db->columnExists($table, 'options')) {
+            $this->renameColumn($table, 'options', 'settings');
+        }
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m180515_000002_rename_notification_options_column cannot be reverted.\n";
+        return false;
+    }
+}
