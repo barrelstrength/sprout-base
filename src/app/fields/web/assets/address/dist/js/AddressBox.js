c5ad44ade4c283c4c50990a3ee4ac6b813b1e00a
Dale Ramirez <dalefirstpage@gmail.com>
2018-07-18 21:07:23 +0800

    - Added countryInput and addressForm variable on twig template. - changed addressForm call by finding it on the template - Removed countryCodeHtml and formInputHtml

--- src/app/fields/web/assets/address/dist/js/AddressBox.js
+++ src/app/fields/web/assets/address/dist/js/AddressBox.js
@@ -95,7 +95,7 @@ if (typeof Craft.SproutBase === typeof undefined) {
                 this.$addressFormat.removeClass('hidden');
             }
 
-            this.$addressForm = $("<div class='sproutaddress-form hidden' />").appendTo(this.$addressBox);
+            this.$addressForm = this.$addressBox.find('.sproutaddress-form');
 
             this._getAddressFormFields();
 
@@ -198,9 +198,6 @@ if (typeof Craft.SproutBase === typeof undefined) {
                 this.$addressBox.find('.address-format .spinner').remove();
                 self.$addressBox.find('.address-format').empty();
                 self.$addressBox.find('.address-format').append(response.html);
-                self.$addressForm.empty();
-                self.$addressForm.append(response.countryCodeHtml);
-                self.$addressForm.append(response.formInputHtml);
 
             }, this));
         },
