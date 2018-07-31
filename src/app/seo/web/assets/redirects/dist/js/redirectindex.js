2acd78aab9534440a1e543715d44cf7085d64f03
Andre L <andrelopezd@gmail.com>
2018-07-31 11:35:54 -0400

    Clean comments in js.

--- src/app/seo/web/assets/redirects/dist/js/redirectindex.js
+++ src/app/seo/web/assets/redirects/dist/js/redirectindex.js
@@ -12,18 +12,6 @@
         {
             var $siteMenu = $('.sitemenubtn:first').menubtn().data('menubtn').menu;
 
-            // Upload file on click
-            $('.translations-upload-button').click(function() {
-                $('input[name="translations-upload"]').click().change(function() {
-                    $(this).parent('form').submit();
-                });
-            });
-
-            // Init the form
-            if(Craft.getLocalStorage('BaseElementIndex.siteId')) {
-                //$siteIdInput.val(Craft.getLocalStorage('BaseElementIndex.siteId'));
-            }
-
             // Change the siteId when on hidden values
             $siteMenu.on('optionselect', function(ev) {
                 var uri = '';
@@ -34,7 +22,6 @@
                         $("#sprout-seo-new-button").attr("href", uri);
                     }
                 }
-               // $siteIdInput.val($(ev.selectedOption).data('siteId'));
             });
         },
     });
