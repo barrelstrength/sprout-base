13a8a19f8a8a70708128e1ca51803b73fa6494cc
Ben Parizek <ben@barrelstrengthdesign.com>
2018-09-10 19:15:28 -0400

    Fixes broken new report URLs

--- src/app/reports/base/DataSource.php
+++ src/app/reports/base/DataSource.php
@@ -168,7 +168,7 @@ abstract class DataSource
     {
         $pluginHandle = Craft::$app->getRequest()->getSegment(1);
 
-        $baseUrl = $pluginHandle.'/reports/' . $this->dataSourceId . '/';
+        $baseUrl = $pluginHandle.'/reports/';
 
         $appendedUrl = ltrim($append, '/');
 
