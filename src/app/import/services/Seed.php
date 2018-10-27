c6833c3ccd19866d4830593767d7b2d0e1f5ed34
Ben Parizek <ben@barrelstrengthdesign.com>
2018-10-27 08:55:59 -0400

    Removes die statement

--- src/app/import/services/Seed.php
+++ src/app/import/services/Seed.php
@@ -112,7 +112,7 @@ class Seed extends Component
                         $row = [];
 
                         $row['@model'] = $seed['type'];
-                        Craft::dd($row);
+
                         /**
                          * @var $importerClass ElementImporter|SettingsImporter
                          */
