53e5255dd451352f7efee76cb9e12bfdfc5b3dc0
Ben Parizek <ben@barrelstrengthdesign.com>
2019-06-16 23:39:53 -0400

    Adds landing page base template and feature component macros

---
+++ src/web/assets/landing/LandingAsset.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @link      https://sprout.barrelstrengthdesign.com/
+ * @copyright Copyright (c) Barrel Strength Design LLC
+ * @license   http://sprout.barrelstrengthdesign.com/license
+ */
+
+namespace barrelstrength\sproutbase\web\assets\landing;
+
+use craft\web\AssetBundle;
+use craft\web\assets\cp\CpAsset as CraftCpAsset;
+
+class LandingAsset extends AssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->sourcePath = '@sproutbase/web/assets/landing/dist';
+
+        $this->depends = [
+            CraftCpAsset::class
+        ];
+
+        $this->css = [
+            'css/landing.css',
+        ];
+
+        parent::init();
+    }
+}
