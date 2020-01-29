236d6b6805c72f1d1786f149506f168c026bc4af
Ben Parizek <ben.parizek@gmail.com>
2020-01-28 22:38:23 -0500

    Adds addSubNavIcon method

--- src/services/Utilities.php
+++ src/services/Utilities.php
@@ -21,4 +21,32 @@ class Utilities extends Component
             Craft::$app->getQueue()->push($purgeElementsJob);
         }
     }
+
+    /**
+     * Adds open in new window icon to $nth li tag of subnav, finding it by label
+     *
+     * @param string $pluginHandle
+     * @param string $navLabel
+     */
+    public function addSubNavIcon(string $pluginHandle, string $navLabel)
+    {
+        $js = <<<EOD
+(function() {
+    var navItem = $('#nav-{$pluginHandle}').find('li a');
+    var cssSelector = null;
+    navItem.each(function(index) {
+        if ($(this).text().trim() === '{$navLabel}') {
+        nth = index + 1;
+        cssSelector = "#nav-{$pluginHandle} .subnav li:nth-child("+nth+") a:after";
+        additionalCss = '<style>'+cssSelector+' { font-family: "Craft"; content: "circlerarr"; margin-left: 5px; }</style>';
+        console.log(additionalCss);
+        $('head').append(additionalCss);
+        return false;
+        }
+    });
+})();
+EOD;
+
+        Craft::$app->getView()->registerJs($js);
+    }
 }
