94453d29c8c37e2cbba320ff81543f18e9c9fce3
Ben Parizek <ben.parizek@gmail.com>
2020-07-02 00:52:54 -0400

    recompile assets

--- src/web/assets/src/js/campaigns/CopyPaste.ts
+++ src/web/assets/src/js/campaigns/CopyPaste.ts
@@ -1,18 +1,19 @@
+declare var Craft: any;
+
 $(document).on('sproutModalBeforeRender', function(event, content) {
   $('.btnSelectAll', content).off().on('click', function(event) {
 
     event.preventDefault();
 
-    $this = $(event.target);
-    $target = '#' + $this.data('clipboard-target-id');
-    $message = $this.data('success-message');
+    let $this = $(event.target);
+    let $target = '#' + $this.data('clipboard-target-id');
+    let $message = $this.data('success-message');
 
-    $content = $($target).select();
+    let $content = $($target).select();
 
     // Copy our selected text to the clipboard
     document.execCommand("copy");
 
     Craft.cp.displayNotice($message);
   });
-
 });
