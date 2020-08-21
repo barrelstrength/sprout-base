5844a5042051c81f618b68115a8bdd0f912bf913
Ben Parizek <ben.parizek@gmail.com>
2020-08-21 08:53:47 -0400

    Migrate db settings to project config

---
+++ src/migrations/m200700_000000_migrate_settings_to_project_config.php
@@ -0,0 +1,75 @@
+<?php /** @noinspection PhpDeprecationInspection */
+
+namespace barrelstrength\sproutbase\migrations;
+
+use Craft;
+use craft\db\Migration;
+use craft\db\Query;
+
+class m200700_000000_migrate_settings_to_project_config extends Migration
+{
+    public function safeUp(): bool
+    {
+        $oldSettings = (new Query())
+            ->select([
+                'model',
+                'settings',
+            ])
+            ->from(['{{%sprout_settings}}'])
+            ->all();
+
+        $settingsMap = [
+            'barrelstrength\sproutbasereports\models\Settings' => 'reports',
+            'barrelstrength\sproutbasesentemail\models\Settings' => 'sent-email',
+            'barrelstrength\sproutbaseemail\models\Settings' => 'email',
+
+            // @todo - Review siteSettings and groupSettings formatting
+            'barrelstrength\sproutbasesitemaps\models\Settings' => 'sitemaps',
+            'barrelstrength\sproutbaseredirects\models\Settings' => 'redirects',
+        ];
+
+        // Disable "Show Upgrade Messages" by default for folks when upgrading.
+        // so Sprout Forms users don't all of a sudden see a bunch of upgrade stuff
+        $cpSettings = [
+            'enableUpgradeMessages' => false,
+        ];
+
+        foreach ($oldSettings as $settingsGroup) {
+
+            if (!isset($settingsMap[$settingsGroup['model']])) {
+                // Log NotFound
+                continue;
+            }
+
+            $moduleKey = $settingsMap[$settingsGroup['model']];
+            $configKey = 'plugins.sprout.'.$moduleKey;
+            $newSettings = $settingsMap[$settingsGroup['oldSettings']];
+
+            // Build the CP modules settings
+            $cpSettings['modules'][] = [
+                'alternateName' => $newSettings['pluginNameOverride'],
+                'enabled' => 1,
+                'moduleKey' => $moduleKey,
+            ];
+
+            unset($newSettings['pluginNameOverride']);
+
+            // @todo - do we need to pack any of the settings?
+            // ProjectConfigHelper::packAssociativeArrays($siteSettings)
+
+            Craft::$app->getProjectConfig()->set($configKey, $newSettings, "Update Sprout Settings for “{$configKey}”");
+        }
+
+        Craft::$app->getProjectConfig()->set('plugins.sprout.control-panel', $cpSettings, 'Update Sprout Settings for “plugins.sprout.control-panel”');
+
+        // delete sprout_settings table
+        $this->dropTableIfExists('{{%sprout_settings}}');
+    }
+
+    public function safeDown(): bool
+    {
+        echo "m200700_000000_migrate_settings_to_project_config cannot be reverted.\n";
+
+        return false;
+    }
+}
